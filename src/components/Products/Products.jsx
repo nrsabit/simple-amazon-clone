da35b238a2bc87b886b28e35156cb77cab847add
nrsabit <mailbox.sabit@gmail.com>
2023-03-28 05:07:07 +0600

    products added

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,6 +4,7 @@ import './Products.css'
 const Products = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
+        // if we are loading some items from public folder, then we don't need to write the path, we can only write the file name, and if we put some item in the public folder, then it will never be changed, otherwise it can be changed
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data));
