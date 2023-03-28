5d13c1b636376fb401f1db685a7bfabd515453e3
nrsabit <mailbox.sabit@gmail.com>
2023-03-29 02:59:59 +0600

    first module completed

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+    const {name, price, img, ratings, seller} = props.product;
+    const handleCartItems = props.handleCartItems;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <h6 className="product-title">{name}</h6>
+                <p className='product-price'>Price: ${price}</p>
+                <p className='seller'>Manufecturer: {seller}</p>
+                <p className="rating">Ratting: {ratings} Star</p>
+            </div>
+            <button className='btn-add-cart' onClick={() => handleCartItems(props.product)}>Add to Cart</button>
+        </div>
+    );
+};
+
+export default Product;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,24 +1,39 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Products.css'
 
 const Products = () => {
+    const [cart, setCart] = useState([]);
     const [products, setProducts] = useState([]);
     useEffect(() => {
-        // if we are loading some items from public folder, then we don't need to write the path, we can only write the file name, and if we put some item in the public folder, then it will never be changed, otherwise it can be changed
         fetch('products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data));
+            .then(res => res.json())
+            .then(data => setProducts(data));
     }, [])
+    const handleCartItems = product =>{
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
     return (
         <div className='products-container'>
             <div className='products-section'>
-                <h2>Products will come here: {products.length}</h2>
+                {
+                    products.map(product => <Product key={product.id} product={product} handleCartItems={handleCartItems}></Product>)
+                }
             </div>
             <div className='cart-section'>
-                <h3>The Cart will be applied here</h3>
+                <h3>Order Summary</h3>
+                <p>Total Cart Items: {cart.length}</p>
             </div>
         </div>
     );
 };
 
 export default Products;
+
+
+ // if we are loading some items from public folder, then we don't need to write the path, we can only write the file name, and if we put some item in the public folder, then it will never be changed, otherwise it can be changed
+ 
+//  IMPORTANT: we all know that the react is unidirectional, that means we can send send data from parent to child but we can't send data from child to parent, but there is a way, and by using that we can send data from child to parent. and that is: just declare a function in the parent with parameters if needed, and then send that function to the child as parameter (props) then you will find the function in the child element in props (we can send the functions also as props and we can send anything) now you can't edit the props so you will have to store it inside another variable, and in child element now if you call the variable that contains the function, it will call the function from parent element and you can pass parameters in that function if needed, so it will be received in the parent element so like this, we can send data as function's parameters from child to parent. 
+
+// you can't push into useState's parameter which will return the data because that is not immutable, so you will have to copy that into another array and then you can push into that and then just set again into useState by the 2nd parameter function. 
